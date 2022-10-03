10a69d32c7f5585d5d1dd1f68363b05e8211cf2e
Livya Kendi Kiruja <livyakendi1@gmail.com>
2022-10-03 20:35:24 +0300

    lab complete

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,4 @@
 class Author < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    validates :phone_number, numericality: {only_integer: true}, length: { is: 10 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+    validate :clickbait?
+
+    BAIT_CHECK = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+      ]
+
+    def clickbait?
+        if BAIT_CHECK.none? { |check | check.match title }
+        errors.add(:title, "must be clickbait")
+      end
+    end
+
 end
